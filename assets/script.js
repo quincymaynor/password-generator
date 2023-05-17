23aaa8bbf6fb9c69620ab3999f57ab714bba315b
Quincy Maynor <131222776+quincymaynor@users.noreply.github.com>
2023-05-17 10:22:28 -0500

    condensed all of the queries into one big function and added an object to capture all of the reponses

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,70 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var passwordLengthQuery = function() {
-    var passwordLength = window.prompt("Select a password length between 8 and 128 characters")
-}
-
-var lowerCaseQuery = function() {
-    // Ask user if they want lowercase characters
-    var  lowerCaseConfirm= window.confirm("Do you want your password to contain lowercase characters?");
-
-    // If user pressed yes, add lowercase array to the password generating array
-    if (lowerCaseConfirm) {
-    somethingaboutaddinglowercasecharacterstothearrayforgeneratingthepassword();
-    }
-}
-
-var upperCaseQuery = function() {
-    // Ask user if they want uppercase characters
-    var  upperCaseConfirm= window.confirm("Do you want your password to contain uppercase characters?");
-
-    // If user pressed yes, add uppercase array to the password generating array
-    if (upperCaseConfirm) {    somethingaboutaddinguppercasecharacterstothearrayforgeneratingthepassword();
-    }
-}
-
-var numericQuery = function() {
-    // Ask user if they want numeric characters
-    var  numericConfirm= window.confirm("Do you want your password to contain numeric characters?");
-
-    // If user pressed yes, add numeric array to the password generating array
-    if (numericConfirm) {
-    somethingaboutaddingnumericcharacterstothearrayforgeneratingthepassword();
-    }
-}
-
-var specialCharacterQuery = function() {
-    // Ask user if they want special characters
-    var  spcialCharacterConfirm= window.confirm("Do you want your password to contain special characters?");
-
-    // If user pressed yes, add special character array to the password generating array
-    if (specialCharacterConfirm) {
-    somethingaboutaddingspecialcharacterstothearrayforgeneratingthepassword();
-    }
-}
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+
+function passwordQueries() {
+    // Ask user for length of password
+    var passwordLength = window.prompt("Select a password length between 8 and 128 characters");
+
+    parseInt(passwordLength, 10) //might need to be set equal to a variable
+
+    if (passwordLength < 8) {
+        return "Error: password length is too short"
+    } else if (passwordLength > 128) {
+        return "Error: password length is too long"
+    } else {
+        "Your password will be " + (passwordLength) + " characters long"
+    }
+
+    // Ask user if they want lowercase characters
+    var  lowerCaseConfirm = window.confirm("Do you want your password to contain lowercase characters?");
+
+    /* If user pressed yes, add lowercase array to the password generating array?
+    if (lowerCaseConfirm) {
+    addlowercasecharacterstothearrayforgeneratingthepassword?();
+    }*/
+
+    // Ask user if they want uppercase characters
+    var  upperCaseConfirm = window.confirm("Do you want your password to contain uppercase characters?");
+
+    /* If user pressed yes, add uppercase array to the password generating array?
+    if (upperCaseConfirm) {    adduppercasecharacterstothearrayforgeneratingthepassword?();
+    }*/
+
+    // Ask user if they want numeric characters
+    var  numericConfirm = window.confirm("Do you want your password to contain numeric characters?");
+
+    /* If user pressed yes, add numeric array to the password generating array?
+    if (numericConfirm) {
+    addnumericcharacterstothearrayforgeneratingthepassword?();
+    }*/
+
+    // Ask user if they want special characters
+    var  specialCharacterConfirm = window.confirm("Do you want your password to contain special characters?");
+
+    /* If user pressed yes, add special character array to the password generating array?
+    if (specialCharacterConfirm) {
+    addspecialcharacterstothearrayforgeneratingthepassword?();
+    }*/
+
+    // object containing all of the query answers
+    var userOptions = {
+        length: passwordLength,
+        hasLowerCase: lowerCaseConfirm,
+        hasUpperCase: upperCaseConfirm,
+        hasNumeric: numericConfirm,
+        hasSpecialCharacter: specialCharacterConfirm,
+    }
+    return userOptions
+}
+
+//randomized element funtion
+
+//generate password function with boolean statements
 
 // Write password to the #password input
 function writePassword() {
