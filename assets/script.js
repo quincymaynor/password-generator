1d722578a6f5d4afca93aecb9a8808c624b3c294
Quincy Maynor <131222776+quincymaynor@users.noreply.github.com>
2023-05-18 22:54:14 -0500

    updated README

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+var specialCharacters = ["!", "\"", "#", "\$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
 
 // Ask user for length of password
 function passwordLength() {
@@ -87,7 +87,7 @@ function generatePassword() {
   }
 
   console.log(masterArray);
-  
+
   generatedArray = [];
 
   //randomized character assignment
