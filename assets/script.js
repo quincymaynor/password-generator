60c614c624b1121fafe3ac0084e76aae6c6154a8
Quincy Maynor <131222776+quincymaynor@users.noreply.github.com>
2023-05-18 20:37:42 -0500

    fixed the functions, added the basic array construction logic

--- assets/script.js
+++ assets/script.js
@@ -1,82 +1,105 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 
 // Ask user for length of password
 function passwordLength() {
-    window.prompt("Select a password length between 8 and 128 characters");
+    var length = window.prompt("Select a password length between 8 and 128 characters");
 
-    passwordInteger = parseInt(passwordLength, 10) //might need to be set equal to a variable
-
-    if (passwordInteger < 8) {
+    var passwordSize = parseInt(length);
+  
+    if (passwordLength < 8) {
     window.alert("Error: password length is too short");
     return;
-    } else if (passwordInteger > 128) {
+    } else if (passwordLength > 128) {
     window.alert("Error: password length is too long");
     return;
     } else {
-    window.alert("Your password will be " + passwordInteger + " characters long");
+    window.alert("Your password will be " + length + " characters long");
     }
+    return passwordSize;
 }
 
 // Ask user if they want lowercase characters
 function lowerCaseConfirm() {
-    window.confirm("Do you want your password to contain lowercase characters?");
+    var confirm = window.confirm("Do you want your password to contain lowercase characters?");
+    var lowerCase = confirm
+    return lowerCase;
 }
 
 // Ask user if they want uppercase characters
 function upperCaseConfirm() {
-    window.confirm("Do you want your password to contain uppercase characters?");
-} 
-    
+    var confirm = window.confirm("Do you want your password to contain uppercase characters?");
+    var upperCase = confirm
+    return upperCase;
+}
+        
 // Ask user if they want numeric characters
 function numericConfirm() {
-    window.confirm("Do you want your password to contain numeric characters?");
+    var confirm = window.confirm("Do you want your password to contain numeric characters?");
+    var numeric = confirm
+    return numeric;
 }
 
 // Ask user if they want special characters
 function specialCharacterConfirm() {
-    window.confirm("Do you want your password to contain special characters?");
+    var confirm = window.confirm("Do you want your password to contain special characters?");
+    var specialCharacter = confirm
+    return specialCharacter;
 }
 
 
-function passwordQueries() {
+function generatePassword() {
 
-    passwordLength();
-    /*if (!passwordLength) {
-        return;
-    }*/
+  var passwordlength = passwordLength();
+  var lowerCase = lowerCaseConfirm();
+  var upperCase = upperCaseConfirm();
+  var numeric = numericConfirm();
+  var specialCharacter = specialCharacterConfirm();
 
-    lowerCaseConfirm();
+  console.log(passwordlength);
+  console.log(lowerCase);
+  console.log(upperCase);
+  console.log(numeric);
+  console.log(specialCharacter);
 
-    upperCaseConfirm();
+  var masterArray = {
+    lc: [],
+    uc: [],
+    nc: [],
+    sc: [],
+  }
 
-    numericConfirm();
+  // Logic for creating master array
 
-    specialCharacterConfirm();
+  if (lowerCase) {
+    masterArray.lc.push(lowerCaseLetters);
+  }
 
+  if (upperCase) {
+    masterArray.uc.push(upperCaseLetters);
+  }
+
+  if (numeric) {
+    masterArray.nc.push(numbers);
+  }
+  
+  if (specialCharacter) {
+    masterArray.sc.push(specialCharacters);
+  }
+
+  console.log(masterArray);
+
+  //randomized element funtion
+  var index = Math.floor(Math.random() * masterArray.length);
+  var passwordText = masterArray[index];
+
+  return generatePassword;
 }
-
-passwordQueries();
-
-// object containing all of the query answers
-var userOptions = {
-    length: passwordInteger,
-    hasLowerCase: lowerCaseConfirm,
-    hasUpperCase: upperCaseConfirm,
-    hasNumeric: numericConfirm,
-    hasSpecialCharacter: specialCharacterConfirm,
-}
-
-console.log(userOptions);
-
-//randomized element funtion
-
-//generate password function with boolean statements
 
 // Write password to the #password input
 function writePassword() {
@@ -86,6 +109,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
